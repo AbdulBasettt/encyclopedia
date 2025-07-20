d9a71bfce0f9ba7ff60b62468bac96b62d2b345c
AbdulBasettt <abdibaset96@gmail.com>
2025-07-20 16:48:08 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js').then(reg => {
-      console.log('Service Worker registered.', reg);
-    });
-  });
-}
-document.getElementById('app').innerText = "Добро пожаловать в энциклопедию!";
