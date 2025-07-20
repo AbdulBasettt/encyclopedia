bdf9d7da1fd4bc647b78509c05a972e30a1e5e36
AbdulBasettt <abdibaset96@gmail.com>
2025-07-20 16:47:20 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function toggleTheme() {
+  document.body.classList.toggle('dark');
+}
+function toggleLanguage() {
+  alert("Смена языка пока не реализована");
+}
+function toggleSettings() {
+  alert("Настройки профиля скоро будут добавлены");
+}
+document.getElementById("content").innerText = "Добро пожаловать в энциклопедию!";
