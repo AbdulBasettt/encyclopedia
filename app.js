a1f43acbb2dfdfba90bc53b36988294a721214f1
AbdulBasettt <abdibaset96@gmail.com>
2025-07-24 18:00:27 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const content = document.getElementById('content');
+  content.textContent = 'Добро пожаловать в Энциклопедию Абдула!';
+  // Здесь будет логика фильтрации, избранного, офлайн и обновлений
+});
