e7e2adc5ae804f4c8bf8fb437d11fcc7e4189179
AbdulBasettt <abdibaset96@gmail.com>
2025-07-25 21:09:00 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-import './settings.js';
-import './player.js';
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("open-settings").addEventListener("click", () => {
-    alert("Настройки пока не реализованы");
-  });
-
-  document.getElementById("content").textContent = "Добро пожаловать в энциклопедию!";
-});
+// Основной JS логика будет позже
